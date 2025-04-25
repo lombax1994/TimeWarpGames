b7b0b15fc5ed6ed0d28ebeb9be22c693a2de94ea
demuy <demuy@LAPTOP-6EPS763E>
2025-04-25 23:39:51 +0200

    homepage aangepast Kleine UI changes

--- TimeWarpGames.Webapp/Controllers/GamesController.cs
+++ TimeWarpGames.Webapp/Controllers/GamesController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
 using TimeWarpGames.Bll;
 using TimeWarpGames.Entities;
 
@@ -13,17 +14,18 @@ namespace TimeWarpGames.Webapp.Controllers
 {
     public class GamesController : Controller
     {
-        // GET: Games
+        //met de index methode maken we een lijst van games aan om weer te geven in de view
         public ActionResult Index()
         {
             try
             {
+                //We vragen de games op bij de bll
                 List<Game> lstGames = TimeWarpGames.Bll.GameBll.ReadAll();
                 return View(lstGames);
             }
             catch (Exception ex)
             {
-                // Log the exception (not implemented here)
+                //Als er problemen zijn dan gaan we naar de errorpagina.
                 ViewBag.ErrorMessage = "Er is een fout opgetreden bij het ophalen van de spellen.";
                 return View("Error");
             }
@@ -31,9 +33,13 @@ namespace TimeWarpGames.Webapp.Controllers
 
         public ActionResult Create()
         {
+            // Deze create methode maakt enkel de Create view aan, hier kunnen we gegevens invullen
             return View();
         }
 
+
+        //Dit is de effectieve create methode
+        //Hier worden de gegevens die we hebben ingevuld in de create view naar de database gestuurd
         [HttpPost]
         public ActionResult Create(string Name, bool IsBoxed, HttpPostedFileBase Image, string Description,
             decimal Price,
@@ -68,13 +74,16 @@ namespace TimeWarpGames.Webapp.Controllers
             return View();
         }
 
+        //Met de details methode maken we een view van een game aan
         public ActionResult Details(int id)
         {
+            //We vragen de game op bij de bll
             try
             {
                 Game game = TimeWarpGames.Bll.GameBll.ReadOne(id);
                 return View(game);
             }
+            //Als er problemen zijn dan gaan we naar de errorpagina.
             catch (Exception ex)
             {
                 ViewBag.ErrorMessage = "Er is een fout opgetreden bij het ophalen van de spelgegevens.";
@@ -82,6 +91,8 @@ namespace TimeWarpGames.Webapp.Controllers
             }
         }
 
+        //Met de delete methode maken we een view van een game aan
+        //Hier kunnen we de game verwijderen
         public ActionResult Delete(int id)
         {
             try
@@ -96,6 +107,9 @@ namespace TimeWarpGames.Webapp.Controllers
             }
         }
 
+
+        //Dit is de effectieve delete methode
+        //Als je op de delete view op de delete knop drukt, dan wordt deze methode aangeroepen
         [HttpPost]
         public ActionResult Delete(string id)
         {
@@ -112,8 +126,7 @@ namespace TimeWarpGames.Webapp.Controllers
             }
         }
 
-        // GET: Games/Edit/5
-        // GET: Games/Edit/5
+        //Hier wordt de edit view aangemaakt
         public ActionResult Edit(int id)
         {
             try
@@ -121,38 +134,44 @@ namespace TimeWarpGames.Webapp.Controllers
                 Game game = TimeWarpGames.Bll.GameBll.ReadOne(id);
                 if (game == null)
                 {
-                    // If no game is found with the given id, return HttpNotFound
-                    return HttpNotFound();
+                    //als er geen spel wordt gevonden gaan we naar de errorpagina
+                    return View("Error");
                 }
 
+                // We geven de game door aan de view
                 return View(game);
             }
             catch (Exception ex)
             {
-                // In case of any errors (e.g., DB connection failure), show an error page
+                // Als er nog andere problemen zijn gaan we ook naar de error pagina
                 ViewBag.ErrorMessage = ex.Message;
                 return View("Error");
             }
         }
 
 
-        // POST: Games/Edit/5
+        //dit is de effectieve edit methode
+        //Hier worden de gegevens die we hebben ingevuld in de edit view naar de database gestuurd
+        //Als je op de edit view op de edit knop drukt, dan wordt deze methode aangeroepen
+        //Image is hier geen string maar een HttpPostedFileBase
+        //Dit is een bestand dat we kunnen uploaden
+
         [HttpPost]
         public ActionResult Edit(int id, string Name, bool IsBoxed, HttpPostedFileBase Image, string Description,
             decimal Price, int Stock, Platform Platform, Genre Genre, string Developer, DateTime ReleaseDate, int AgeRating)
         {
             string ImageName;
 
-            // Retrieve the existing game
+            //We vragen de game op bij de bll
             Game existingGame = TimeWarpGames.Bll.GameBll.ReadOne(id);
             if (existingGame == null)
             {
-                return HttpNotFound(); // Return 404 if the game doesn't exist
+                return View("Error"); //Als het spel niet gevonden wordt, gaan we naar de errorpagina
             }
 
-            ImageName = existingGame.Image; // Keep the existing image if not replaced
+            ImageName = existingGame.Image; // Als de image niet wordt vervangen, behouden we de oude naam
 
-            // If a new image is uploaded, handle the file saving
+            // Hier controleren we of er een nieuwe afbeelding is geüpload
             if (Image != null)
             {
                 if (Image.ContentType == "image/jpeg" || Image.ContentType == "image/png")
@@ -164,20 +183,24 @@ namespace TimeWarpGames.Webapp.Controllers
                 }
             }
 
-            // Call the BLL to update the game
+            //Hier geven we de aangepaste gegevens door aan de bll
             bool gameUpdated = TimeWarpGames.Bll.GameBll.Update(id, Name, IsBoxed, ImageName, Description, Price, Stock,
                 Platform, Genre, Developer, ReleaseDate, AgeRating);
 
+            //Hier controleren we of het spel is bijgewerkt
+            //Als het spel is bijgewerkt, geven we een feedback weer
             if (gameUpdated)
             {
                 ViewBag.Feedback = Name + " is bijgewerkt";
             }
+            //Als het spel niet is bijgewerkt, geven we een feedback weer
             else
             {
                 ViewBag.Feedback = "Bijwerken van spel is niet gelukt";
             }
 
-            return View(TimeWarpGames.Bll.GameBll.ReadOne(id)); // Re-fetch and display the updated game
+
+            return RedirectToAction("Details", new{id = id}); // Re-fetch and display the updated game
         }
 
 
