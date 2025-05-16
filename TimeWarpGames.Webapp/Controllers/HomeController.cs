808d06e630367160236e79dc655835b9a821ca28
demuy <demuy@LAPTOP-6EPS763E>
2025-05-16 10:25:48 +0200

    Privacy Page Added

--- TimeWarpGames.Webapp/Controllers/HomeController.cs
+++ TimeWarpGames.Webapp/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace TimeWarpGames.Webapp.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Privacy()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
